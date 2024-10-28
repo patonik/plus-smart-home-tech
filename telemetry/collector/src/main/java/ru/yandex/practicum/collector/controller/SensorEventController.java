a944e2f8e45fabecadda63657c32ce8c7d528cac
NikolayPatov <nikolay.patov@gmail.com>
2024-10-28 09:55:46 +0300

    fix: change configs to use custom serialization implemented in according kafka templates

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/SensorEventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/SensorEventController.java
@@ -1,31 +1,30 @@
 package ru.yandex.practicum.collector.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.collector.event.sensor.SensorEvent;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/sensors")
 public class SensorEventController {
 
     private final KafkaTemplate<String, SensorEvent> kafkaTemplate;
     private static final String SENSOR_TOPIC = "telemetry.sensors.v1";
 
     @Autowired
-    public SensorEventController(KafkaTemplate<String, SensorEvent> kafkaTemplate) {
+    public SensorEventController(@Qualifier("sensorEventKafkaTemplate") KafkaTemplate<String, SensorEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @PostMapping("/sensors")
+    @PostMapping
     public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
-        // Determine the sensor type and log it
-        System.out.println("Received sensor event of type: " + event.getType());
-
-        // Send the sensor event to the Kafka topic for sensor data
         kafkaTemplate.send(SENSOR_TOPIC, event.getId(), event);
     }
 }
